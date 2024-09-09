01bcfe3a78d8d83fe0ed76aa8b1c49461e1e2049
TyliaBridges <tylia.bridges@sciacademy.org>
2024-09-09 15:12:43 +0000

    message

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,21 +43,23 @@ $(function () {
     createPlatform(45, 700, 200, 20); 
     createPlatform(60,250,300,30);
     createPlatform(700, 500, 600,30);
+    createPlatform(900,400 ,20, 100);
+    createPlatform(1050, 400, 20, 100);
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
 
-
-
+createCollectable("database", 1200,400); //create a "grace" collectible at the coordinates(500,300) falling with default gravity and bouncing with default bounce %
+createCollectable("database", 55,600);
     
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+createCannon("top", 700, 600); // cannon on left wall, 600px down, shooting once per second
 
 
     
